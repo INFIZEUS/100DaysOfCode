35f5e44eb893de9449500f2a6356512d7d31579d
Lakshit <lakshitjain.jain2003@gmail.com>
2022-09-27 19:44:27 +0530

    Day 3 commit

---
+++ Day 3/src/com/lakshit/Main.java
@@ -0,0 +1,17 @@
+package com.lakshit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    public static int[] buildArray(int[] arr) {
+        int[] ans = new int[]{arr.length};
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
+}
