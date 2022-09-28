8c5ba2d7a0a0e0ae03633f3a6ac11d11aa083dac
Lakshit <lakshitjain.jain2003@gmail.com>
2022-09-28 23:59:48 +0530

    Day 4 commit

---
+++ Day 4/src/com/lakshit/Main.java
@@ -0,0 +1,24 @@
+package com.lakshit;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = {1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(arr));
+
+    }
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
